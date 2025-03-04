6096f6c2b9d1f52a8f701065616efc1229652a8a
Pedro Olsen Ferreira <silverclaw@users.noreply.github.com>
2025-03-04 21:18:32 +0000

    Add missing include
    Fixes build in modern compilers.

--- lib/testbench.cpp
+++ lib/testbench.cpp
@@ -13,6 +13,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with CLTestbench.  If not, see <https://www.gnu.org/licenses/>.
 
+#include <algorithm>
 #include <cassert>
 #include <iostream>
 
